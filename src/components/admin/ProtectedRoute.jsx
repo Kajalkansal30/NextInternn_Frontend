6b418ad72ad4d3e997fc5a4801bc65832e5aa29d
Kajalkansal30 <144597783+Kajalkansal30@users.noreply.github.com>
2025-05-13 18:05:53 +0530

    Update ProtectedRoute.jsx

--- src/components/admin/ProtectedRoute.jsx
+++ src/components/admin/ProtectedRoute.jsx
@@ -1,17 +1,36 @@
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+
+// const ProtectedRoute = ({ children }) => {
+//   const user = useSelector(state => state.auth.user);
+
+//   if (!user) {
+//     // Not logged in, redirect to login page
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   // Logged in, render children components
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const user = useSelector(state => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
+  const location = useLocation();
 
   if (!user) {
-    // Not logged in, redirect to login page
-    return <Navigate to="/login" replace />;
+    // Redirect to login page if user is not authenticated
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  // Logged in, render children components
   return children;
 };
 
 export default ProtectedRoute;
+
